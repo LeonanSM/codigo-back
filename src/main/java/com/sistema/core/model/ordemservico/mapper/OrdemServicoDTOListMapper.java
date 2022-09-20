bce9591b2a5bcdbbbb781ced5a97c5a13e206da7
Leonan Machado <leonanprogramador@outlook.com>
2022-09-20 17:03:17 -0300

    20/09/2022

--- src/main/java/com/sistema/core/model/ordemservico/mapper/OrdemServicoDTOListMapper.java
+++ src/main/java/com/sistema/core/model/ordemservico/mapper/OrdemServicoDTOListMapper.java
@@ -5,12 +5,8 @@ import org.mapstruct.Mapper;
 import com.sistema.core.model.abstractions.BaseMapper;
 import com.sistema.core.model.ordemservico.OrdemServico;
 import com.sistema.core.model.ordemservico.dto.OrdemServicoDTOList;
-import com.sistema.core.model.ordemservicoitem.mapper.OrdemServicoItemListDTOMapper;
-import com.sistema.core.model.pessoa.mapper.PessoaListDTOMapper;
 
-@Mapper(componentModel = "spring",uses= {OrdemServicoItemListDTOMapper.class, PessoaListDTOMapper.class})
+@Mapper(componentModel = "spring")
 public interface OrdemServicoDTOListMapper extends BaseMapper<OrdemServico, OrdemServicoDTOList> {
-
-
 
 }
--- src/test/java/com/sistema/core/TestePessoa.java
+++ src/test/java/com/sistema/core/TestePessoa.java
@@ -1,43 +1,12 @@
 package com.sistema.core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.sistema.core.model.pessoa.Pessoa;
-import com.sistema.core.model.pessoa.PessoaRepository;
 
 
 
 @SpringBootTest
 public class TestePessoa {
 
-	@Autowired
-	private PessoaRepository repository;
 	
-	@Test
-	public void testaPesquisaExata() {
-
-		Optional<Pessoa> optional = repository.findByNome("Joao");
-		
-		assertEquals(true, optional.isPresent());
-	}
-	
-	
-	@Test
-	public void testaPesquisaLike() {
-
-		List<Pessoa> lista = repository.findByNomeContaining("o");
-		
-		for (Pessoa pessoa : lista) {
-			
-			System.out.println(pessoa.getNome());
-		}
-	}
 }
 	
